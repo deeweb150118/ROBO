85b640102f1b2e01ee94a58707ec1a337beb6268
deepanshu-kav <62233903+deepanshu-kav@users.noreply.github.com>
2020-04-07 14:27:39 +0530

    Updates

---
+++ precache-manifest.8836e5a90064d094d8e53fc1d66203cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8daa4f797994ed7c3355c3bfdfdd7fc",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/robo/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "59c5540f18da92a55633",
+    "url": "/robo/static/css/main.dec07e0c.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/robo/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c5540f18da92a55633",
+    "url": "/robo/static/js/main.94ff1ac2.chunk.js"
+  },
+  {
+    "revision": "d076c46e4f4970d5fa6a",
+    "url": "/robo/static/js/runtime-main.5e1c8283.js"
+  }
+]);
